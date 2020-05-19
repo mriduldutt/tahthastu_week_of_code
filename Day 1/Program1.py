071bf3b4ca7d2e12d2c011f4271f00893df66ecc
mriduldutt <65560761+mriduldutt@users.noreply.github.com>
2020-05-19 22:51:15 +0530

    Create Program1.py

---
+++ Day 1/Program1.py
@@ -0,0 +1,42 @@
+name=input("enter your Name")
+branch=input("enter your Branch")
+gender=input("enter your Gender")
+college=input("enter your College")
+age=int(input("enter your Age"))
+print("your Name is ", name)
+print("your Branch is ", branch)
+print("your Gender is ",gender)
+print("your College is ",college)
+print("your Age is ",age)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
