730c81d26d8186d845ca457945ff90d10f62d27b
mriduldutt <65560761+mriduldutt@users.noreply.github.com>
2020-05-19 23:08:40 +0530

    Create Program 5.py

---
+++ Day 1/Program 5.py
@@ -0,0 +1,15 @@
+run1=int(input("Enter run of the player 1 "))
+run2=int(input("Enter run of the player 2 "))
+run3=int(input("Enter run of the player 3 "))
+strikeRate1=run1*100 / 60
+strikeRate2=run2*100 / 60
+strikeRate3=run3*100 / 60
+print("strikeRate of player 1 is ",strikeRate1)
+print("strikeRate of player 2 is ",strikeRate2)
+print("strikeRate of player 3 is ",strikeRate3)
+print("When player 1 played 60 balls more ",run1*2)
+print("When player 2 played 60 balls more ",run2*2)
+print("When player 3 played 60 balls more ",run3*2)
+print("max. no of six player 1 could have ",run1//6)
+print("max. no of six player 2 could have ",run2//6)
+print("max. no of six player 3 could have ",run3//6)
