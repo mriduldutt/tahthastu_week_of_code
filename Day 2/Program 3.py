6abe09ecb53aebc75081beb67245dc1f2edd8f73
mriduldutt <65560761+mriduldutt@users.noreply.github.com>
2020-05-21 23:23:04 +0530

    Create Program 3.py

---
+++ Day 2/Program 3.py
@@ -0,0 +1,29 @@
+n=int(input("enter the no. :-"))
+for i in range(n):
+    print(" "*i + "*" + "  "*(n-1-i) + "*")
+for i in range(n):
+    print(" "*(n-1-i) + "*" + "  "*i + "*")
+
+
+    
+
+
+   
+
+
+    
+
+
+
+
+    
+
+
+
+
+
+    
+
+
+
+   
