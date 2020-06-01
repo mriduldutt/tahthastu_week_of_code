a70380654d587bbf58eb710cbce40e81c5107441
mriduldutt <65560761+mriduldutt@users.noreply.github.com>
2020-06-01 10:23:41 +0530

    Create Program 4.py

---
+++ day 4/Program 4.py
@@ -0,0 +1,27 @@
+n=int(input('Enter dictionary size :-'))
+dic={}
+dictionary1={}
+for j in range(n):
+    key=int(input('Enter the '+str(j+1)+'key'))
+    val=int(input('Enter the '+str(j+1)+'val'))
+    dic[key]=val
+    if( dic[key] not in list(dictionary1.val())):
+        dictionary1[key]=val
+print(dictionary1)
+
+
+
+
+
+
+
+    
+
+    
+
+    
+
+    
+
+        
+
