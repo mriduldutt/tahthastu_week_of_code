10b7270746a3eb8116e4e623421c6577f05a88b0
mriduldutt <65560761+mriduldutt@users.noreply.github.com>
2020-06-01 10:10:55 +0530

    Create Program 1.py

---
+++ day 4/Program 1.py
@@ -0,0 +1,4 @@
+n=int(input('Enter  size of tuple :-'))
+tup=tuple(map(int,input('Enter the element seprated by space:').split()))
+ele=int(input('Enter the element to find it occurence:'))
+print(tup.count(ele)
